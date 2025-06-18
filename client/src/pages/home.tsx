3ef65d1788b00f3c1985088413ec03eab6683bfc
Akashkatageri <d.katageri360@gmail.com>
2025-06-18 17:11:00 +0530

    Update home.tsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,7 +1,7 @@
 import Navigation from "@/components/navigation";
 import Hero from "@/components/hero";
 import About from "@/components/about";
-import Skills from "@/components/skills";
+import { Skills } from "@/components/skills";
 import Projects from "@/components/projects";
 import Contact from "@/components/contact";
 import Footer from "@/components/footer";
